872593c3ccfe39088b0e57b40e91ca9889aaae2e
Iwan Pavord <ipavord@gmail.com>
2026-05-19 13:50:05 +0100

    more o2-tpd and h2-tpr

--- migrate.py
+++ migrate.py
@@ -1,6 +1,6 @@
 from src.db import Migrator, DB
 
-file_path = "src/db/migrations/15-5-26.json"
+file_path = "src/db/migrations/18-5-26.json"
 
 db = DB()
 migrator = Migrator(
@@ -9,5 +9,5 @@ migrator = Migrator(
     staging_dir="src/db/migrations/staged_microscopy",
     migration_failure_dir="src/db/migrations/failed_migrations",
 )
-# migrator.migrate_file(file_path)
-migrator.backfill_subcreations_by_fingerprint(file_path)
+migrator.migrate_file(file_path)
+# migrator.backfill_subcreations_by_fingerprint(file_path)
